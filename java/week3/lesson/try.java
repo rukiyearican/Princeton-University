af0cfe02de5f62b64573d444e5efa6af8d011337
Rukiye Arıcan <100317918+rukiyearican@users.noreply.github.com>
2022-12-21 01:47:38 +0300

    Add files via upload

---
+++ java/week3/lesson/try.java
@@ -0,0 +1,12 @@
+public class try 
+{
+    public static void main(String[] args) 
+    {
+        double[] a;
+        double[] b = new double[a.length];
+        for (int i=0; i < a.length; i++)
+        {
+            b[i] = a[i];
+        }
+    }
+}
